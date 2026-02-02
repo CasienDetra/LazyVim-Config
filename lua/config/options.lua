4a46aed5d5f504a5625e56a25471e0c39f296143
CasienDetra <sarath.rithyyanouk.1223@rupp.edu.kh>
2026-02-02 11:46:22 +0700

    refactor(cmp) change from blink to nvim-cmp
    yes sir

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.lazyvim_cmp = "blink.cmp"
+-- vim.g.lazyvim_cmp = "blink.cmp"
 vim.o.pumblend = 0
 vim.o.winblend = 0
 vim.opt.clipboard = "unnamedplus"
