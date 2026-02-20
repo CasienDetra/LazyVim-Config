f06ae82478c0f8300ee1db8034bcbbe5ff8d91a1
ArtemisCasien <nuk385916@gmail.com>
2026-02-20 21:17:22 +0700

    more tweak

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -44,12 +44,52 @@ return {
   },
   -- catppuccin
   {
-    {
-      "catppuccin/nvim",
-      lazy = false,
-      priority = 1000,
-      opts = {
-        transparent_background = true,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    build = ":CatppuccinCompile",
+    enabled = true,
+    opts = {
+      flavour = "macchiato",
+      background = { light = "latte", dark = "macchiato" },
+      transparent_background = true,
+      float = { transparent = true, solid = true },
+      term_colors = true,
+      custom_highlights = function(C)
+        local O = require("catppuccin").options
+        return {
+          ["@module"] = { fg = C.lavender, style = O.styles.miscs or { "italic" } },
+          ["@type.builtin"] = { fg = C.yellow, style = O.styles.properties or { "italic" } },
+          ["@property"] = { fg = C.lavender, style = O.styles.properties or {} },
+        }
+      end,
+      integrations = {
+        bufferline = false,
+        cmp = true,
+        fidget = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        markdown = true,
+        mason = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        neotree = true,
+        noice = true,
+        notify = true,
+        rainbow_delimiters = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
       },
     },
   },
