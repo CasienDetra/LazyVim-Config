f3f690a3c566eac8f02abb81c1026fa331f6ebf7
CasienDetra <sarath.rithyyanouk.1223@rupp.edu.kh>
2026-03-19 08:46:42 +0700

    style : change nerd icon

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -245,7 +245,7 @@ return {
             require("gitsigns").setup({
                 signs = {
                     add = { text = "┆" },
-                    change = { text = "↴" },
+                    change = { text = "➜" },
                     delete = { text = "󰍵" },
                     topdelete = { text = "‾" },
                     changedelete = { text = "~" },
