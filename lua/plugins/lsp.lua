c582f8b627c5c3621eaff9412607d95d09fc5b8c
ArtemisCasien <nuk385916@gmail.com>
2026-04-20 20:10:25 +0700

    chore(lsp) : not using tiny diagnostic

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -177,31 +177,31 @@ return {
             })
         end,
     },
-    {
-        "rachartier/tiny-inline-diagnostic.nvim",
-        event = "VeryLazy",
-        priority = 1000,
-        config = function()
-            require("tiny-inline-diagnostic").setup({
-                preset = "powerline",
-                show_source = { enabled = true },
-                multilines = { enabled = true, always_show = true },
-                use_icons_from_diagnostic = true,
-                show_all_diags_on_cursorline = true,
-                -- Only show diagnostics when the cursor is directly over them, no fallback to line diagnostics
-                show_diags_only_under_cursor = false,
-                set_arrow_to_diag_color = false,
-                signs = {
-                    left = "",
-                    right = "",
-                    diag = "λ",
-                    arrow = "  ",
-                    up_arrow = "  ",
-                    vertical = " │",
-                    vertical_end = " ╰>",
-                },
-            })
-            vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
-        end,
-    },
+    -- {
+    --     "rachartier/tiny-inline-diagnostic.nvim",
+    --     event = "VeryLazy",
+    --     priority = 1000,
+    --     config = function()
+    --         require("tiny-inline-diagnostic").setup({
+    --             preset = "powerline",
+    --             show_source = { enabled = true },
+    --             multilines = { enabled = true, always_show = true },
+    --             use_icons_from_diagnostic = true,
+    --             show_all_diags_on_cursorline = true,
+    --             -- Only show diagnostics when the cursor is directly over them, no fallback to line diagnostics
+    --             show_diags_only_under_cursor = false,
+    --             set_arrow_to_diag_color = false,
+    --             signs = {
+    --                 left = "",
+    --                 right = "",
+    --                 diag = "λ",
+    --                 arrow = "  ",
+    --                 up_arrow = "  ",
+    --                 vertical = " │",
+    --                 vertical_end = " ╰>",
+    --             },
+    --         })
+    --         vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+    --     end,
+    -- },
 }
